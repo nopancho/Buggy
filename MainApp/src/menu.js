9effe894eda437bfb575147ac9bf1bf394da74c4
s.sprenger <s.sprenger@zoovu.com>
2024-12-20 13:57:09 +0100

    icon fix

--- MainApp/src/menu.js
+++ MainApp/src/menu.js
@@ -1,8 +1,5 @@
 import {getRouterInstance}  from "../../jsModules/src/baseRouter";
 
 export default {
-    logout(){
-        document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-        window.location = 'http://localhost:3000/#/login';
-    },
+
 }
